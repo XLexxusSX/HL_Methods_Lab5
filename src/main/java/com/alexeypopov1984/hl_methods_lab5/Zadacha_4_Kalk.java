8bb5c3ad264b119353ed9708c066a6edfcc2a61b
Alexey.Popov <lexxuss2008@gmail.com>
2026-01-04 06:02:55 +0700

    Лаба 5. Добавил поле для обнуления и соответствующую кнопку C

--- src/main/java/com/alexeypopov1984/hl_methods_lab5/Zadacha_4_Kalk.java
+++ src/main/java/com/alexeypopov1984/hl_methods_lab5/Zadacha_4_Kalk.java
@@ -9,12 +9,33 @@ import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.stage.Modality;
 import javafx.geometry.Insets;
+import javafx.scene.layout.GridPane;
 
 import javafx.stage.Stage;
 
 public class Zadacha_4_Kalk {
+    private static boolean startNewNumber = true;
+    private static double firstNumber = 0;
+    private static String operation = "";
+    private static TextField display = new TextField();
+
+
     public static void showWindow(Stage owner) {
-        VBox vbox = new VBox(10);
+
+        Button[] numberButtons = new Button[10];
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new Button(String.valueOf(i));
+            int num = i;
+            numberButtons[i].setOnAction(e -> appendNumber(String.valueOf(num)));
+        }
+
+        Button clearButton = new Button("C");
+        clearButton.setOnAction(e -> clear());
+
+        GridPane grid = new GridPane();
+        grid.add(clearButton, 0, 4, 4, 1);
+
+        VBox vbox = new VBox(10, display, grid);
         vbox.setAlignment(Pos.CENTER_LEFT);
         vbox.setPadding(new Insets(20));
         Stage stage = new Stage();
@@ -29,5 +50,19 @@ public class Zadacha_4_Kalk {
         stage.setY((owner.getHeight() - stage.getHeight()) / 2);
     }
 
-    
+    private static void appendNumber(String number) {
+        if (startNewNumber) {
+            display.setText(number);
+            startNewNumber = false;
+        } else {
+            display.setText(display.getText() + number);
+        }
+    }
+    private static void clear() {
+        display.setText("0");
+        firstNumber = 0;
+        operation = "";
+        startNewNumber = true;
+    }
+
 }
