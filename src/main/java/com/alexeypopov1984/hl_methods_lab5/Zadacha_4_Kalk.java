5b45a9597435fc08075ca65ab9582a238335c58a
Alexey.Popov <lexxuss2008@gmail.com>
2026-01-04 17:10:48 +0700

    Лаба 5. Калькуляр. Пока без дробных чисел работает

--- src/main/java/com/alexeypopov1984/hl_methods_lab5/Zadacha_4_Kalk.java
+++ src/main/java/com/alexeypopov1984/hl_methods_lab5/Zadacha_4_Kalk.java
@@ -14,9 +14,9 @@ import javafx.geometry.Insets;
 import javafx.stage.Stage;
 
 public class Zadacha_4_Kalk {
-    private static TextField textFieldForNums = new TextField();
+    private static final TextField textFieldForNums = new TextField();
     private static double firstNumber = 0;
-    private static String operation = "";
+    private static String thisOperation = "";
     private static boolean startNewNumber = true;
 
     public static void showWindow(Stage owner) {
@@ -37,12 +37,43 @@ public class Zadacha_4_Kalk {
         Button clearButton = new Button("C");
         clearButton.setOnAction(e -> clear());
 
+
+        Button plusButton = new Button("+");
+        plusButton.setOnAction(e -> setOperation("+"));
+
+        Button minusButton = new Button("-");
+        minusButton.setOnAction(e -> setOperation("-"));
+
+        Button multButton = new Button("*");
+        multButton.setOnAction(e -> setOperation("*"));
+
+        Button divButton = new Button("/");
+        divButton.setOnAction(e -> setOperation("/"));
+
+        Button equalsButton = new Button("=");
+        equalsButton.setOnAction(e -> equals());
+
+
         grid.add(numberButtons[0], 0, 0);
         grid.add(numberButtons[1], 1, 0);
         grid.add(numberButtons[2], 2, 0);
 
-        VBox vbox = new VBox(10, textFieldForNums, grid, clearButton);
-        vbox.setAlignment(Pos.CENTER_LEFT);
+        grid.add(numberButtons[3], 0, 1);
+        grid.add(numberButtons[4], 1, 1);
+        grid.add(numberButtons[5], 2, 1);
+
+        grid.add(numberButtons[6], 0, 2);
+        grid.add(numberButtons[7], 1, 2);
+        grid.add(numberButtons[8], 2, 2);
+
+        grid.add(clearButton, 0, 3);
+        grid.add(plusButton, 1, 3);
+        grid.add(minusButton, 2, 3);
+
+        //grid.add(clearButton, 0, 4, 4, 1);
+
+        VBox vbox = new VBox(10, textFieldForNums, grid, equalsButton, multButton, divButton);
+        vbox.setAlignment(Pos.CENTER);
         vbox.setPadding(new Insets(20));
         Stage stage = new Stage();
         Scene scene = new Scene(vbox, 400, 250);
@@ -54,6 +85,34 @@ public class Zadacha_4_Kalk {
         stage.show();
         stage.setX((owner.getWidth() - stage.getWidth()) / 2);
         stage.setY((owner.getHeight() - stage.getHeight()) / 2);
+    }
+
+    private static void equals() {
+
+        double secondNumber = Double.parseDouble(textFieldForNums.getText());
+        double result = 0;
+
+        switch (thisOperation) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                if (secondNumber == 0) {
+                    textFieldForNums.setText("Ошибка: деление на 0");
+                    return;
+                }
+                result = firstNumber / secondNumber;
+                break;
+        }
+
+        textFieldForNums.setText(String.valueOf(result));
+        startNewNumber = true;
     }
 
     private static void appendNum (String num){
@@ -68,7 +127,13 @@ public class Zadacha_4_Kalk {
     private static void clear() {
         textFieldForNums.setText("0");
         firstNumber = 0;
-        operation = "";
+        thisOperation = "";
+        startNewNumber = true;
+    }
+
+    private static void setOperation(String operation) {
+        firstNumber = Double.parseDouble(textFieldForNums.getText());
+        thisOperation = operation;
         startNewNumber = true;
     }
 }
