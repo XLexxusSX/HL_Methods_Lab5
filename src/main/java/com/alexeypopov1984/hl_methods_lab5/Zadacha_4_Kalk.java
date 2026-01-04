97d28e405ed8c62c5ec52b347fa4b6f1629b06fb
Alexey.Popov <lexxuss2008@gmail.com>
2026-01-04 18:37:33 +0700

    Лаба 5. Калькуляр. Практически всё готово, запишем изменения

--- src/main/java/com/alexeypopov1984/hl_methods_lab5/Zadacha_4_Kalk.java
+++ src/main/java/com/alexeypopov1984/hl_methods_lab5/Zadacha_4_Kalk.java
@@ -21,58 +21,83 @@ public class Zadacha_4_Kalk {
 
     public static void showWindow(Stage owner) {
 
-        GridPane grid = new GridPane();
-        grid.setHgap(5);
-        grid.setVgap(5);
-        grid.setAlignment(Pos.CENTER);
-
         Button[] numberButtons = new Button[10];
 
         for (int i = 0; i < 10; i++) {
             numberButtons[i] = new Button(String.valueOf(i));
             int num = i;
             numberButtons[i].setOnAction(e -> appendNum(String.valueOf(num)));
+            numberButtons[i].setMinWidth(40);
         }
 
         Button clearButton = new Button("C");
         clearButton.setOnAction(e -> clear());
+        clearButton.setMinWidth(40);
 
+        Button dotButton = new Button(".");
+        dotButton.setOnAction(e -> appendNum("."));
+        dotButton.setMinWidth(40);
 
         Button plusButton = new Button("+");
         plusButton.setOnAction(e -> setOperation("+"));
+        plusButton.setMinWidth(40);
 
         Button minusButton = new Button("-");
         minusButton.setOnAction(e -> setOperation("-"));
+        minusButton.setMinWidth(40);
 
         Button multButton = new Button("*");
         multButton.setOnAction(e -> setOperation("*"));
+        multButton.setMinWidth(40);
 
         Button divButton = new Button("/");
         divButton.setOnAction(e -> setOperation("/"));
+        divButton.setMinWidth(40);
 
         Button equalsButton = new Button("=");
         equalsButton.setOnAction(e -> equals());
+        equalsButton.setMinWidth(40);
 
+        Button plusMinusButton = new Button("+/-");
+        plusMinusButton.setOnAction(e -> plusMinus());
+        plusMinusButton.setMinWidth(40);
 
-        grid.add(numberButtons[0], 0, 0);
-        grid.add(numberButtons[1], 1, 0);
-        grid.add(numberButtons[2], 2, 0);
+        GridPane grid = new GridPane();
+        grid.setHgap(25);
+        grid.setVgap(25);
+        grid.setAlignment(Pos.CENTER);
 
-        grid.add(numberButtons[3], 0, 1);
-        grid.add(numberButtons[4], 1, 1);
-        grid.add(numberButtons[5], 2, 1);
+        grid.add(numberButtons[7], 0, 0);
+        grid.add(numberButtons[8], 1, 0);
+        grid.add(numberButtons[9], 2, 0);
+        grid.add(divButton,        3, 0);
 
-        grid.add(numberButtons[6], 0, 2);
-        grid.add(numberButtons[7], 1, 2);
-        grid.add(numberButtons[8], 2, 2);
+        grid.add(numberButtons[4], 0, 1);
+        grid.add(numberButtons[5], 1, 1);
+        grid.add(numberButtons[6], 2, 1);
+        grid.add(multButton,       3, 1);
 
-        grid.add(clearButton, 0, 3);
-        grid.add(plusButton, 1, 3);
-        grid.add(minusButton, 2, 3);
+        grid.add(numberButtons[1], 0, 2);
+        grid.add(numberButtons[2], 1, 2);
+        grid.add(numberButtons[3], 2, 2);
+        grid.add(minusButton,      3, 2);
+
+        grid.add(plusMinusButton,  0, 3);
+        grid.add(numberButtons[0], 1, 3);
+        grid.add(dotButton,        2, 3);
+        grid.add(plusButton,       3, 3);
+
+        grid.add(clearButton, 0, 4);
+
+//        gridSec.add(equalsButton, 0, 0);
+//        gridSec.add(multButton, 1, 0);
+//        gridSec.add(divButton, 2, 0);
+//        gridSec.add(dotButton, 3, 0);
+//        gridSec.add(plusMinusButton, 4, 0);
 
         //grid.add(clearButton, 0, 4, 4, 1);
 
-        VBox vbox = new VBox(10, textFieldForNums, grid, equalsButton, multButton, divButton);
+        VBox vbox = new VBox(10, textFieldForNums, grid);
         vbox.setAlignment(Pos.CENTER);
         vbox.setPadding(new Insets(20));
         Stage stage = new Stage();
@@ -135,5 +160,16 @@ public class Zadacha_4_Kalk {
         firstNumber = Double.parseDouble(textFieldForNums.getText());
         thisOperation = operation;
         startNewNumber = true;
+    }
+    
+    private static void plusMinus(){
+        String currentText = textFieldForNums.getText();
+        if (!currentText.equals("0")) {
+            if (currentText.startsWith("-")) {
+                textFieldForNums.setText(currentText.substring(1));
+            } else {
+                textFieldForNums.setText("-" + currentText);
+            }
+        }
     }
 }
